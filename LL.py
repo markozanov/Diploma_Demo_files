126226c22f5536da9e750e79ff26798c7b64f9db
Виктор Маркозанов <viktor.markozanov@students.finki.ukim.mk>
2020-10-30 20:36:33 +0100

    v2

--- LL.py
+++ LL.py
@@ -3,12 +3,14 @@ w = open("../demoGBLLResult.txt", "w")
 
 results = []
 
+x = 0
 while True:
     line = f.read(10)
     if line == "":
         break
     numbers = line.split(",")
-    print(numbers)
+    x += 1
+    print(str(numbers) + " " + str(x))
     SUM = int(numbers[0]) + int(numbers[1])
     AVG = SUM/2
     w.write(str(SUM) + "," + str(AVG))
